eef9fdaf1324de57dcffa75d2c5506ff6e20cccf
masterdhruvsharma-bit <masterdhruvsharma@gmail.com>
2025-09-28 12:16:09 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,61 @@
+package com.skillconnect.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password; // Only used for registration/login, not stored as plain text
+    private String bio;
+
+    // --- Getters and Setters ---
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
